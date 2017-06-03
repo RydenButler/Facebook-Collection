d18e6fb060955aad841c50ae845c2ac2c3dae4fb
Ryden W. Butler <r.butler@wustl.edu>
2017-06-02 19:04:00 -0500

    Bug Fixes and SQL Update
    - Update version of GraphAPI in SenateLikerVerbose.py
    - Add “Maintype Error” catches in SenateLikerVerbose.py
    - Removed senator URLs from code file
    - Updated code to include latest SQLSaver.py

---
+++ UpdatedCode/SenatorScraping/SQLSaver.py
@@ -0,0 +1,103 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+  try:
+    conn = sqlite3.connect(db_file)
+    return conn
+  except Error as e:
+    print(e)
+ 
+  return None
+
+
+def create_table(conn, create_table_sql):
+  try:
+    c = conn.cursor()
+    c.execute(create_table_sql)
+  except Error as e:
+    print(e)
+
+
+def create_database(database_name):
+ 
+  sql_create_ID_table = """ CREATE TABLE IF NOT EXISTS ID (
+                            id integer PRIMARY KEY,
+                            name text NOT NULL,
+                            Facebook_id text UNIQUE
+                            ); """
+ 
+  sql_create_Data_table = """CREATE TABLE IF NOT EXISTS Data (
+                            id integer PRIMARY KEY,
+                            WUSTL_id integer NOT NULL,
+                            page text NOT NULL,
+                            message text NOT NULL,
+                            message_id text NOT NULL,
+                            post_time text NOT NULL,
+                            FOREIGN KEY (WUSTL_id) REFERENCES ID (id)
+                            );"""
+ 
+  # create a database connection
+  conn = create_connection(database_name)
+  if conn is not None:
+    # create ID table
+    create_table(conn, sql_create_ID_table)
+    # create data table
+    create_table(conn, sql_create_Data_table)
+  else:
+    print("Error! cannot create the database connection.")
+
+
+def create_ID(conn, user):
+  sql = ''' INSERT OR IGNORE INTO ID(name, Facebook_id)
+            VALUES(?,?) '''
+  cur = conn.cursor()
+  cur.execute(sql, user)
+  cur.execute('SELECT rowid FROM ID WHERE Facebook_id = ?', (user[1],))
+  return cur.fetchone()[0]
+
+def create_Data(conn, data): 
+  sql = ''' INSERT INTO Data(WUSTL_id, page, message, message_id, post_time)
+            VALUES(?,?,?,?,?) '''
+  cur = conn.cursor()
+  cur.execute(sql, data)
+  return cur.lastrowid
+
+def check_existing_user(conn, user):
+  cur = conn.cursor()
+  cur.execute("SELECT * FROM ID WHERE Facebook_id=?", (user,))
+  rows = cur.fetchall()
+  for row in rows:
+    return(row)
+
+
+def populate_database(giant_tree, database_name):
+  # create a database connection
+  conn = create_connection(database_name)
+  tally = 0
+  with conn:
+  # Iterate over facebook page names
+    for page in giant_tree.keys():
+      # Iterate over posts on a facebook page
+      for post in giant_tree[page]:
+        # Iterate over individual likes on a post
+        for liker in post['likes']:
+          tally += 1
+          print 'Storing data for observation %d' % (tally)
+          try:
+            # Store name and id of liker
+            user = (liker['name'], liker['id']);
+          except KeyError, name:
+            # If no name field, record placeholder
+            user = ('NO NAME FIELD', liker['id']);
+          user_id = create_ID(conn, user)
+          print 'Current row id is %s' % (user_id)
+          try:
+            like_data = (user_id, page, post['message'], post['id'], post['created_time'])
+          except KeyError, message:
+            try:
+              like_data = (user_id, page, post['story'], post['id'], post['created_time'])
+            except KeyError, story:
+              like_data = (user_id, page, 'NO MESSAGE CONTENT', post['id'], post['created_time'])
+          create_Data(conn, like_data)
+  conn.close()
--- UpdatedCode/SenatorScraping/ScrapeSenators.py
+++ UpdatedCode/SenatorScraping/ScrapeSenators.py
@@ -1,9 +1,11 @@
 import csv
 import re
+import sys
+sys.path.insert(0, 'Facebook-Project/UpdatedCode/SenatorScraping')
 
 # Collect urls from csv of senator information
 senator_pages = []
-with open('SenatorURLs.csv', 'rb') as urls:
+with open('Data/SenateData/Scraping/SenatorURLs.csv', 'rb') as urls:
   reader = csv.reader(urls)
   for row in reader:
     senator_pages.extend(row)
@@ -16,15 +18,28 @@ from SenateLikerVerbose import *
 #from SenateLiker import *
 
 # Running this function requires:
-#   1) Navigating to the SenatorScraping directory in the terminal.
-#   2) Picking one of the above scrapers to user. The verbose one fills your terminal with updates. Otherwise they are identical.
-#   3) Reading an app id and app secret from the Graph API into python.
+#   1) Navigating to the main project directory in the terminal. For me: cd Dropbox/Facebook
+#   2) Picking one of the above scrapers to user. The verbose one fills your terminal with updates. 
+#      They are otherwise functionally identical.
+#   3) Read an app id and app secret from the Graph API into python.
 #   4) Setting the function's arguments ID and SECRET to the correspoding object names of you app id and secret
 # The first senator, Luther Strange, doesn't work for some reason through the API; this is not an indexing error
-data = like_scraper(senator_pages[1:], ID, SECRET)
+results = like_scraper(senator_pages[0:2], APP_ID, APP_SECRET)
+data = results['Data']
+bad_pages = results['Errors']
 
+# Quickly check that the data looks okay
 data['RichardShelby'][1]
 
-from DataSaver import *
+# Choose whether to save data as .db or .csv
+# For .db (SQL)
+from SQLSaver import *
+
+create_database('Likes.db')
+
+populate_database(data, 'Likes.db')
+
+# For .csv
+from CSVSaver import *
 
 make_csv(data)
